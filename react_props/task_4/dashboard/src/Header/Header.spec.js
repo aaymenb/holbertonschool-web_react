b6fa65a634a9facc6f2bde4ab75d6c6a8a48ab45
aaymenb <aymen1996ben@gmail.com>
2026-05-25 14:47:15 +0200

    ...

--- react_props/task_4/dashboard/src/Header/Header.spec.js
+++ react_props/task_4/dashboard/src/Header/Header.spec.js
@@ -2,19 +2,24 @@ import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
 describe('Header component', () => {
-  test('renders the Holberton logo image', () => {
-    render(<Header />);
-    const logo = screen.getByAltText(/holberton logo/i);
+  test('renders the Holberton logo', () => {
+    const { container } = render(<Header />);
+    const logo = container.querySelector('img');
     expect(logo).toBeInTheDocument();
     expect(logo.tagName).toBe('IMG');
+    expect(screen.getByAltText(/holberton logo/i)).toBe(logo);
   });
 
-  test('renders the h1 heading with the text "School dashboard"', () => {
-    render(<Header />);
+  test('renders an h1 heading with the text "School dashboard"', () => {
+    const { container } = render(<Header />);
+    const title = container.querySelector('h1');
     const heading = screen.getByRole('heading', {
       level: 1,
-      name: /school dashboard/i,
+      name: 'School dashboard',
     });
     expect(heading).toBeInTheDocument();
+    expect(title).toBe(heading);
+    expect(heading.tagName).toBe('H1');
+    expect(heading).toHaveTextContent('School dashboard');
   });
 });
